116eeb62875688458cda0cc2c45ae64baf4432ba
Dan Galano <mooncoreee25@gmail.com>
2024-07-22 20:54:11 +0800

    feature a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:huzzl_web/views/homepage.dart';
 
 void main() {
   runApp(const HuzzlWeb());
@@ -10,7 +11,7 @@ class HuzzlWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/views/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text("Home page")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
