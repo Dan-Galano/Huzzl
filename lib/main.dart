5d19927f2c2461d17be1d8792f7d94a5fcb2daad
Dan Galano <mooncoreee25@gmail.com>
2024-07-24 21:33:03 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:huzzl_web/views/login/login.dart';
+import 'package:huzzl_web/views/recruiters/register/00%20registration_screen.dart';
 import 'package:huzzl_web/views/user%20option/user_option.dart';
 
 void main() {
@@ -13,8 +14,9 @@ class HuzzlWeb extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      // home: LoginPage(),
       // home: UserOptionScreen(),
+      home: RecruiterRegistrationScreen(),
     );
   }
 }
---
+++ lib/views/recruiters/register/00 registration_screen.dart
@@ -0,0 +1,294 @@
+import 'package:flutter/material.dart';
+import 'package:huzzl_web/widgets/navbar/navbar_login_registration.dart';
+
+class RecruiterRegistrationScreen extends StatelessWidget {
+  const RecruiterRegistrationScreen({super.key});
+//modify mo itong login code to a company profile
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Positioned(
+          //   top: 20,
+          //   left: 20,
+          //   child: Image.asset('assets/images/huzzl.png', width: 80),
+          // ),
+          const NavBarLoginRegister(),
+          Center(
+            heightFactor: 1.5,
+            child: Container(
+              width: 670,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Company Profile',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Color(0xff373030),
+                          fontFamily: 'Galano',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Please provide the following to complete company profile.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xff373030),
+                          fontFamily: 'Galano',
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Company Name',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff373030),
+                        fontFamily: 'Galano',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Company Email Address',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff373030),
+                        fontFamily: 'Galano',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Chief Executive Officer',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff373030),
+                        fontFamily: 'Galano',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      hintText: 'First name',
+                      hintStyle: const TextStyle(
+                        color: Color(0xFFB6B6B6),
+                        fontFamily: 'Galano',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff373030),
+                        fontFamily: 'Galano',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD1E1FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      hintText: 'Password (8 or more characters)',
+                      hintStyle: const TextStyle(
+                        color: Color(0xFFB6B6B6),
+                        fontFamily: 'Galano',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        //onpressed
+                      },
+                      child: const Text(
+                        'Forgot password?',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontFamily: 'Galano',
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          //onpressed
+                        },
+                        child: const Text(
+                          'Don\'t have an account? Sign up',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontFamily: 'Galano',
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //onpressed
+                            // Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserOptionScreen(),));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF0038FF),
+                            padding: EdgeInsets.all(20),
+                          ),
+                          child: const Text('LOGIN',
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontFamily: 'Galano',
+                                fontWeight: FontWeight.w700,
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
