3d5f944ada29672257c21ad589fc4d29978e291f
Dan Galano <mooncoreee25@gmail.com>
2024-07-26 11:19:07 +0800

    hiring_manager_profile

---
+++ lib/views/recruiters/register/03 hiring_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HiringManagerProfileScreen extends StatelessWidget {
+  const HiringManagerProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
