866fb586fef5cb2bc04ef7a862c6f7e702aa4dff
allenjalvaro <alllenjjames@gmail.com>
2024-08-01 15:43:02 +0800

    landing page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:huzzl_web/landing%20page/landing_page.dart';
 import 'package:huzzl_web/views/login/login_register.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class HuzzlWeb extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginRegister(),
+      home: LandingPage(),
     );
   }
 }
