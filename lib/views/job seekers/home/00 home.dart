6d859421cb689b15c876dbc0f29f170216ddf35a
monica <avemonica0@gmail.com>
2024-08-10 20:12:08 +0800

    added aggre feature

--- lib/views/job seekers/home/00 home.dart
+++ lib/views/job seekers/home/00 home.dart
@@ -1,13 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:http/http.dart' as http;
+import 'package:huzzl_web/views/job%20seekers/home/home_script.dart';
+import 'package:huzzl_web/views/job%20seekers/home/home_widgets.dart';
 
-class JobSeekerHomeScreen extends StatelessWidget {
+class JobSeekerHomeScreen extends StatefulWidget {
   const JobSeekerHomeScreen({super.key});
+
+  @override
+  State<JobSeekerHomeScreen> createState() => _JobSeekerHomeScreenState();
+}
+
+class _JobSeekerHomeScreenState extends State<JobSeekerHomeScreen> {
+
+  List<Map<String, String>> jobs = [];
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchJobs();
+  }
+
+  Future<void> fetchJobs() async {
+    try {
+      String jobStreetHtml = await fetchJobStreetData();
+      String upworkHtml = await fetchUpworkData();
+      String indeedHtml = await fetchIndeedData();
+      String onlinejobsHtml = await fetchOnlineJobsData();
+      String linkedinHtml = await fetchLinkedInData();
+
+      setState(() {
+        jobs = [
+          ...parseJobStreetData(jobStreetHtml),
+          ...parseUpworkData(upworkHtml),
+          ...parseIndeedData(indeedHtml),
+          ...parseOnlineJobsData(onlinejobsHtml),
+          ...parseLinkedInData(linkedinHtml),
+        ];
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('JOBSEEKER HOMESCREEN'),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  child: Image.asset('assets/images/huzzl.png', width: 80),
+                ),
+                Spacer(),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Home',
+                        style: TextStyle(
+                          color: Color(0xff373030),
+                          fontFamily: 'Galano',
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Company Reviews',
+                        style: TextStyle(
+                          color: Color(0xff373030),
+                          fontFamily: 'Galano',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/message-icon.png', width: 20),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/notif-icon.png', width: 20,),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/user-icon.png', width: 20,),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Colors.grey[400],
+          ),
+
+          Expanded(
+            child: Row(
+              children: [
+                // Sidebar Filters
+                Container(
+                  width: 350,
+                  padding: EdgeInsets.all(30),
+                  child: ListView(
+                    children: [
+                      buildCategoryDropdown(),
+                      SizedBox(height: 16),
+                      buildJobTypeFilter(),
+                      SizedBox(height: 16),
+                      buildClientHistoryFilter(),
+                      SizedBox(height: 16),
+                      buildClientLocationDropdown(),
+                      SizedBox(height: 16),
+                      buildProjectLengthFilter(),
+                    ],
+                  ),
+                ),
+                 Container(
+                  width: 1, 
+                  height: double.infinity, 
+                  color: Colors.grey[300],
+                ),
+                // Job Listings
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        buildSearchBar(),
+                        SizedBox(height: 16),
+                        Expanded(
+                          child: jobs.isEmpty
+                            ? Center(child: CircularProgressIndicator())
+                            : ListView.builder(
+                                itemCount: jobs.length,
+                                itemBuilder: (context, index) {
+                                  return  buildJobCard(
+                                    joblink: jobs[index]['jobLink'] ?? '',
+                                    datePosted: jobs[index]['datePosted'] ?? '',
+                                    title: jobs[index]['title']!,
+                                    location: jobs[index]['location'] ?? '',
+                                    rate: jobs[index]['salary'] ?? '',
+                                    description: jobs[index]['description'] ?? '',
+                                    tags: ["CAD", "Autodesk authCAD", "Electrical Engineering", "Construction"], // SAMPLE TAGS
+                                    // tags: (jobs[index]['tags'] as List<String>?) ?? [],
+                                  );
+                                },
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
