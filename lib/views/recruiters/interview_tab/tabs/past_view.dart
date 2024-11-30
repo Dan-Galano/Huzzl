b99610c6905582813ae8c3f862910bdf01411f62
Dan Galano <mooncoreee25@gmail.com>
2024-11-30 23:58:49 +0800

    video call name, interview tabs done

--- lib/views/recruiters/interview_tab/tabs/past_view.dart
+++ lib/views/recruiters/interview_tab/tabs/past_view.dart
@@ -1,84 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:huzzl_web/views/recruiters/interview_tab/calendar_ui/interview_model.dart';
 import 'package:huzzl_web/views/recruiters/interview_tab/cards/past_interview_card.dart';
+import 'package:huzzl_web/views/recruiters/interview_tab/controller/interview_provider.dart';
+import 'package:provider/provider.dart';
 
-class PastView extends StatelessWidget {
+class PastView extends StatefulWidget {
   PastView({super.key});
 
-  final List interviewees = [
-    {
-      'name': 'Elijah Japheth Macatiag',
-      'profession': 'Photographer',
-      'branch': 'Urdaneta City',
-      'interviewTitle': 'Technical Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-    {
-      'name': 'Jau Salcedo',
-      'profession': 'Web Developer',
-      'branch': 'Alaminos City',
-      'interviewTitle': 'Final Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-    {
-      'name': 'Dan Galano',
-      'profession': 'Web Developer',
-      'branch': 'AlaminosSSSS City',
-      'interviewTitle': 'Screening Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-    {
-      'name': 'Elijah Japheth Macatiag',
-      'profession': 'Photographer',
-      'branch': 'Urdaneta City',
-      'interviewTitle': 'Technical Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-    {
-      'name': 'Jau Salcedo',
-      'profession': 'Web Developer',
-      'branch': 'Alaminos City',
-      'interviewTitle': 'Final Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-    {
-      'name': 'Dan Galano',
-      'profession': 'Web Developer',
-      'branch': 'Alaminos City',
-      'interviewTitle': 'Screening Interview',
-      'dateInterviewed': DateTime(2024, 9, 25, 8, 30),
-      'interviewType': 'Online',
-      'timeRange': DateTimeRange(
-        start: DateTime(2024, 9, 25, 8, 0),
-        end: DateTime(2024, 9, 25, 10, 0),
-      ),
-    },
-  ];
+  @override
+  State<PastView> createState() => _PastViewState();
+}
+
+class _PastViewState extends State<PastView> {
+  late InterviewProvider _interviewProvider;
+
+  List<InterviewEvent> interviewees = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _interviewProvider = Provider.of<InterviewProvider>(context, listen: false);
+
+    fetchPastInterview();
+  }
+
+  void fetchPastInterview() async {
+    await _interviewProvider.fetchPastInterviews();
+    setState(() {
+      interviewees = _interviewProvider.pastInterviews;
+    });
+
+    debugPrint("Fetching the past interview successfully");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,11 +44,7 @@ class PastView extends StatelessWidget {
           itemCount: interviewees.length,
           itemBuilder: (context, index) {
             return PastInterviewTileCard(
-              intervieweeName: interviewees[index]['name'],
-              interviewTitle: interviewees[index]['interviewTitle'],
-              profession: interviewees[index]['profession'],
-              branch: interviewees[index]['branch'],
-              dateInterviewed: interviewees[index]['dateInterviewed'],
+              interview: interviewees[index],
             );
           },
         ),
